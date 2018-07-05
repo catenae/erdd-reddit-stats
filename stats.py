63ed32393ac8f3a515674517990caa3724fad707
brunneis <dev@brunneis.com>
2018-07-06 00:17:39 +0200

    Stats AVG

--- stats.py
+++ stats.py
@@ -17,27 +17,52 @@ class Stats(Link):
 
         self.users_counter = set()
         self.texts_counter = 0
+        self.current_minute = Stats._get_current_minute()
+        self.start_timestamp = Stats._get_current_timestamp()
+        self.messages_left = 5
 
         # Ensure stats index
         self.stats.create_index('timestamp', unique=True, background=True)
 
+    @staticmethod
+    def _get_current_timestamp():
+        return int(round(time.time()))
+
+    @staticmethod
+    def _get_current_minute():
+        return datetime.datetime.fromtimestamp(Stats._get_current_timestamp()).strftime('%M')
+
     def transform(self, electron):
-        try:
-            if electron.previous_topic == 'processed_texts':
-                self.texts_counter += 1
-            elif electron.previous_topic == 'processed_users':
-                self.users_counter.add(electron.key)
+        current_timestamp = Stats._get_current_timestamp()
 
+        if Stats._get_current_minute() > self.current_minute:
+            # Reset counters
+            self.users_counter = set()
+            self.texts_counter = 0
+            self.start_timestamp = current_timestamp
+
+        self.current_minute = Stats._get_current_minute()
+
+        if electron.previous_topic == 'processed_texts':
+            self.texts_counter += 1
+        elif electron.previous_topic == 'processed_users':
+            self.users_counter.add(electron.key)
+
+        # Update values
+        seconds = current_timestamp - self.start_timestamp
+        if seconds > 5 and self.messages_left == 0: # Avoid division by 0 and wait a bit
+            self.messages_left = 5
             values = {}
-            values['texts_second'] = self.texts_counter
-            values['users_second'] = len(self.users_counter)
-
+            if self.texts_counter > 0:
+                values['texts_second'] = self.texts_counter / seconds * 1.
+            if len(self.users_counter) > 0:
+                values['users_second'] = len(self.users_counter) / seconds * 1.
             self.stats.update_one(
                 { 'group': 'real_time' },
                 { '$set': values },
                 upsert=True)
-        except Exception as e:
-            print(e)
+        elif seconds > 5:
+            self.messages_left -= 1
 
 if __name__ == "__main__":
     Stats().start(link_mode=Link.MULTIPLE_KAFKA_INPUTS_CUSTOM_OUPUT, mki_mode='parity')
